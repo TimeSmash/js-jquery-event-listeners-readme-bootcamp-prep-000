56209273e9daa16a414a7f85320ac452a5552fab
mcumm64 <mcumm64@gmail.com>
2019-01-08 21:48:36 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -7,11 +7,13 @@ function getIt() {
 }
 
 function frameIt () {
-  $('')
+  $('img.tasty').on('load', function () {
+    //add red frame
+  })
 }
 
 $(document).ready(function(){
-
+getIt();
 // call functions here
 
 });
